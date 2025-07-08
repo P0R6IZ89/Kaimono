6dd63777d8921005057f613ec42b6482491b3518
P0R6IZ89 <alamsawame@hotmail.com>
2025-07-08 19:37:01 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/",
+        has: [{ type: "host", value: "(?<subdomain>.+)\\.yourdomain\\.com" }],
+      },
+    ];
+  },
   images: {
     domains: ["res.cloudinary.com"],
   },
