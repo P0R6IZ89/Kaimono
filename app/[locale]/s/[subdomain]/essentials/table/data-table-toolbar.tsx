d4eb97dae65238ecbffa65f2a9fb14a4773697fd
p0r6iz89 <alamsawame@hotmail.com>
2025-11-05 11:10:19 +0900

    Updated translations, and improve link handling

--- app/[locale]/s/[subdomain]/essentials/table/data-table-toolbar.tsx
+++ app/[locale]/s/[subdomain]/essentials/table/data-table-toolbar.tsx
@@ -29,7 +29,7 @@ export function DataTableToolbar<TData>({
             <div>
               <DataTableFacetedFilter
                 column={table.getColumn("status")}
-                title="Status"
+                title={t("filter-by-status")}
                 options={statuses}
               />
             </div>
