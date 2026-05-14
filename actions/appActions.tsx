22f1254eeaf5386edd02d9b6655a63476e50a546
p0r6iz89 <alamsawame@hotmail.com>
2026-05-15 00:59:54 +0900

    .

--- actions/appActions.tsx
+++ actions/appActions.tsx
@@ -110,8 +110,11 @@ export async function getAllAppsAction() {
     include: {
       _count: { select: { memberships: true } },
       memberships: {
-        where: { userId: session.user.id },
-        select: { role: true },
+        // where: { userId: session.user.id },
+        select: {
+          role: true,
+          user: { select: { image: true, name: true, email: true } },
+        },
       },
     },
     orderBy: { createdAt: "desc" },
@@ -257,8 +260,7 @@ export async function deleteApp(id: string): Promise<Result> {
     where: { appId_userId: { appId: id, userId: session.user.id } },
     select: { role: true },
   });
-  if (!membership)
-    return { ok: false, message: "notAppMember" };
+  if (!membership) return { ok: false, message: "notAppMember" };
   if (membership.role !== "OWNER")
     return { ok: false, message: "ownerOnlyDeleteApp" };
 
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,10 +3,9 @@ import DeleteDropdown from "@/components/client/deleteDropdown";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import { Plus, UserRound, Users } from "lucide-react";
+import { Link2, Plus, UserRound, Users } from "lucide-react";
 import { getTranslations } from "next-intl/server";
 import { Link } from "@/i18n/navigation";
-import { RelativeTime } from "./components/relativeTime";
 import {
   Item,
   ItemActions,
@@ -18,37 +17,38 @@ import {
 import { UserManager } from "@/components/auth/userManage";
 import { Separator } from "@/components/ui/separator";
 import { protocol, rootDomain, rootDomainHost } from "@/lib/variables";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarGroup,
+  AvatarGroupCount,
+  AvatarImage,
+} from "@/components/ui/avatar";
 
 type PageProps = { params: Promise<{ locale: string }> };
 
 export default async function App({ params }: PageProps) {
   const { locale } = await params;
-  const session = await requireSession();
   const apps = await getAllAppsAction();
   const t = await getTranslations({ locale, namespace: "Teams" });
   return (
     <section className="flex flex-col py-16 px-8 gap-8 min-h-svh mx-auto max-w-3xl justify-center">
       <div className="mx-auto space-y-2">
-        <Item variant={"default"} className="">
+        <Item variant={"default"}>
           <ItemMedia>
             <Users />
           </ItemMedia>
           <ItemContent>
             <ItemTitle>{t("title")}</ItemTitle>
-            <ItemDescription className="line-clamp-none">
-              {t("description")}
-            </ItemDescription>
+            <ItemDescription>{t("description")}</ItemDescription>
           </ItemContent>
         </Item>
-        <UserManager user={session.user} className="w-fit" variant={"muted"} />
+        {/* <UserManager user={session.user} className="w-fit" variant={"muted"} /> */}
       </div>
-
-      <Separator />
-
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 pt-4">
-        <Card className="p-0 gap-3 justify-between outline-1 outline-dashed ring-0 bg-muted/50 border-transparent hover:outline-solid hover:outline-primary/50 hover:bg-accent/50 transition-colors">
-          <Item className="">
-            <ItemContent className="gap-0">
+        <Card className="p-0 justify-center outline-1 outline-dashed ring-0 bg-muted/50 border-transparent hover:outline-solid hover:outline-primary/50 hover:bg-accent/50 transition-colors">
+          <Item>
+            <ItemContent className="items-center text-center justify-center my-auto">
               <ItemTitle>{t("new-team-card.title")}</ItemTitle>
               <ItemDescription>
                 {t("new-team-card.description")}
@@ -75,35 +75,49 @@ export default async function App({ params }: PageProps) {
             >
               <Card className="h-full p-0 gap-3 shadow-md justify-between border-transparent hover:outline-solid hover:outline-primary/50 hover:bg-accent/50 transition-colors">
                 <Item className="">
-                  <ItemContent className="gap-0">
+                  <ItemContent>
                     <ItemTitle>{app.name}</ItemTitle>
-                    <ItemDescription className="text-xs">
-                      <RelativeTime date={app.createdAt} />
-                    </ItemDescription>
-                    <ItemDescription className="text-foreground pt-2">
+                    <ItemDescription className="text-muted-foreground line-clamp-3">
                       {app.description}
                     </ItemDescription>
+                    <AvatarGroup className="pt-2">
+                      {app.memberships.slice(0, 3).map((membership, index) => (
+                        <Avatar key={index} size="sm">
+                          <AvatarImage
+                            src={membership.user.image || undefined}
+                            alt={membership.user.name || undefined}
+                          />
+                          <AvatarFallback>
+                            {membership.user.name
+                              ? membership.user.name.charAt(0).toUpperCase()
+                              : membership.user.email.charAt(0).toUpperCase()}
+                          </AvatarFallback>
+                        </Avatar>
+                      ))}
+
+                      {app._count.memberships - 3 > 0 && (
+                        <AvatarGroupCount>
+                          +{app._count.memberships - 3}
+                        </AvatarGroupCount>
+                      )}
+                    </AvatarGroup>
                   </ItemContent>
+
                   <ItemActions>
                     <DeleteDropdown id={app.id} />
                   </ItemActions>
                 </Item>
 
                 <CardContent className="pb-4">
-                  <div className="flex space-x-2">
-                    <Badge variant={"outline"} className="flex gap-1">
-                      <UserRound absoluteStrokeWidth size={12} />
-                      <p>{app._count.memberships}</p>
-                    </Badge>
-                    <Badge
-                      variant={"outline"}
-                      className="flex flex-1 justify-between"
-                    >
-                      <span className="hover:underline line-clamp-1 truncate">
-                        {app.subdomain}.{rootDomainHost}
-                      </span>
-                    </Badge>
-                  </div>
+                  <Badge
+                    variant={"outline"}
+                    className="flex flex-1 w-full justify-start gap-2"
+                  >
+                    <Link2 />
+                    <span className="hover:underline line-clamp-1 truncate">
+                      {app.subdomain}.{rootDomainHost}
+                    </span>
+                  </Badge>
                 </CardContent>
               </Card>
             </Link>
